31c448f4ab77ed9583889caf404565cc88a8cb1b
Aditya Goyal <adityagoyaldoon@gmail.com>
2021-07-12 14:19:45 -0400

    Much work completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,24 @@ skillsHeader.forEach((el) => {
 })
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]');
+const tabContents = document.querySelectorAll('[data-content]');
 
+tabs.forEach((tab)=>{
+    tab.addEventListener('click', ()=>{
+        let target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active');
+        });
+        target.classList.add('qualification__active');
+
+        tab.forEach(tab =>{
+            tab.classList.remove('qualification__active');
+        })
+        tab.classList.add('qualification__active');
+    })
+});
 
 /*==================== SERVICES MODAL ====================*/
 
